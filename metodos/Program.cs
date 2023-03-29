86aee3d491a3c07b1d2b4e2da58f8923451aae03
gustavo <100374415+projectpimentel@users.noreply.github.com>
2023-03-29 14:36:02 -0300

    Add files via upload

---
+++ metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exemplo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Produto p1 = new Produto();
+
+       //p1.Nome = "parafuso";
+       //p1.Preco = 1.67;
+
+       p1.AlteraNome("Parafuso");
+
+       p1.AlteraPreco(1.98);
+
+       p1.MostraDados();
+    }
+}
